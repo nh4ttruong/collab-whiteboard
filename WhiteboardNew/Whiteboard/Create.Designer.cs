2d607ccd10d1da8136b33a5fd943e85bcfc805cb
nh4ttruong <truongtbn243@gmail.com>
2021-06-06 00:16:07 +0700

    Update!

---
+++ WhiteboardNew/Whiteboard/Create.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Whiteboard
+{
+    partial class Create
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.createARoom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(152, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 28);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("UTM Helve", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(51, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nickname";
+            // 
+            // createARoom
+            // 
+            this.createARoom.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.createARoom.BackColor = System.Drawing.Color.White;
+            this.createARoom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.createARoom.Font = new System.Drawing.Font("UTM Helve", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.createARoom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.createARoom.Location = new System.Drawing.Point(167, 165);
+            this.createARoom.Name = "createARoom";
+            this.createARoom.Size = new System.Drawing.Size(106, 47);
+            this.createARoom.TabIndex = 4;
+            this.createARoom.Text = "CREATE";
+            this.createARoom.UseVisualStyleBackColor = false;
+            this.createARoom.Click += new System.EventHandler(this.createARoom_Click);
+            // 
+            // Create
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(382, 353);
+            this.Controls.Add(this.createARoom);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("UTM Helve", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "Create";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Create";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button createARoom;
+    }
+}
---
+++ WhiteboardNew/Whiteboard/Create.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Whiteboard
+{
+    public partial class Create : Form
+    {
+        string Source_code = "";
+        List<string> Data_code = new List<string>();
+        bool flag_code = true;
+        public Create()
+        {
+            InitializeComponent();
+        }
+
+        private WorkSpace ws = new WorkSpace();
+
+        public delegate void delPassData(String str, TextBox Text);
+
+        private void createARoom_Click(object sender, EventArgs e)
+        {            
+            this.Close();
+            ws.Show();
+            String code = "";
+            String source = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789";
+            Random rd = new Random();
+            int index;
+            while (flag_code)
+            {
+                code = "";
+                for (int i = 0; i < 6; i++)
+                {
+                    index = rd.Next(0, source.Length);
+                    code += source[index];
+                }
+                if (!Data_code.Contains(code))
+                {
+                    flag_code = false;
+                    Data_code.Add(code);
+                }
+            }
+            Source_code = code;          
+            delPassData del = new delPassData(ws.funData);
+            del(code, textBox1);
+        }
+    }
+}
